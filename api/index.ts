5e69c5b17e7e03e1d2aa256cfca4b0a364a35617
DOK <148057045+Pongnakorn16@users.noreply.github.com>
2024-08-10 14:50:44 +0700

    REcent hello world

--- api/index.ts
+++ api/index.ts
@@ -3,5 +3,5 @@ import express from "express";
 export const router = express.Router(); // Router คือตัวจัดการเส้นทาง
 
 router.get("/", (req, res)=>{
-    res.send("Hello world 😎🌏")
+    res.send("Hello World!!! 😎🙌🌏")
 });
